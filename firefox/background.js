ac86a36482da22a0fc00a8b4b600bf0e1948140f
cjc0287 <none@none.com>
2024-06-23 00:57:06 -0500

    add firefox extension

---
+++ firefox/background.js
@@ -0,0 +1,25 @@
+var spotifyUrl = "open.spotify.com";
+var addTitleText = "Advertisement";
+
+function checkTabs(tabId, changeInfo, tab) {
+	if(tab.url.includes(spotifyUrl) && tab.audible){
+		console.log("Inspect Spotify tab change.", tab.title);
+		if(tab.title.includes(addTitleText)){
+			if(!tab.mutedInfo.muted){
+				browser.tabs.update(tab.id, {muted: true});
+				console.log("SOUND OFF");
+			}
+		}
+		else{
+		    if(tab.mutedInfo.muted){
+    			browser.tabs.update(tab.id, {muted: false});
+    			console.log("SOUND ON");
+		    }
+		}
+	}
+};
+
+async function start() {
+	browser.tabs.onUpdated.addListener(checkTabs);		
+}
+start();
